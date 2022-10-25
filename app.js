b9f55e336b742c3aa49db97e1c29350b46a6368d
Santiago Maramer <santiago.maramer00@usc.edu.co>
2022-10-24 23:17:46 -0500

    version 1

---
+++ app.js
@@ -0,0 +1,6 @@
+// Sumar dos numeros 
+
+let num1 = parseInt(prompt("Ingrese el numero 1"));
+let num2= parseInt(prompt("Ingrese el numero 2"));
+let suma = num1 + num2;
+alert("El resultado de su suma es" +suma);
